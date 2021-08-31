61ac05db2bbaec63826cea500af2e1ac5c73ddbc
Changappa_m_a <Changappa_m_a@dell.com>
2021-08-31 16:16:00 +0530

    Section 5 Complete

---
+++ JavaScript/Section5/arraysandobj.js
@@ -0,0 +1,11 @@
+//Array and Objects : collections of Data 
+//Array > mutable
+//[]
+//Array methods > push/pop/shift/unshift
+//Includes and IndexOf
+//join and reserve
+//slice
+//splice
+//sort > by default considers numericals as string
+//Refernce Types > refered to the values memory address 
+//
