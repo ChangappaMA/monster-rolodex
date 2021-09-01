e2221aca015eb72a4cbca36d93e83ccf81ec8331
Changappa_m_a <Changappa_m_a@dell.com>
2021-09-01 12:00:31 +0530

    Section 7 Complete

---
+++ JavaScript/Section7/Loops.js
@@ -0,0 +1,16 @@
+//doing things repeatedly
+//There are multiple types:
+//for loop
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+//While loop
+//break
+//for...of > list iterable
+for (variable of iterable){
+
+}
+//for...in > object iterable
+for (variable in Object){
+
+}
