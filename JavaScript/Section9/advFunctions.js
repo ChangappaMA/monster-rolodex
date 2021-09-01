86207d8c345de2fbb308f55c43ca8e7a37c0f53e
Changappa_m_a <Changappa_m_a@dell.com>
2021-09-01 19:04:14 +0530

    Section 9 Complete

---
+++ JavaScript/Section9/advFunctions.js
@@ -0,0 +1,12 @@
+//Function Scope
+//block scope
+//lexical scope
+//function expressions
+const square = function (num){
+    return num * num
+}
+
+square(9)
+
+//callbacks > function passed into another function as an argument
+//Hoisting
