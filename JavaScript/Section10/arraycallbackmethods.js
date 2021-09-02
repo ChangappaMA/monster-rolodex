4fd597f6c1a8b79e8deb790e0b44c717896b8f8e
Changappa_m_a <Changappa_m_a@dell.com>
2021-09-02 13:51:15 +0530

    Section 10 Complete

---
+++ JavaScript/Section10/arraycallbackmethods.js
@@ -0,0 +1,16 @@
+//forEach
+//map > new array 
+//arrow function
+const square = x => {
+    return x * x;
+}
+//implicit arrow function
+const sqr = n => n * n
+//array.find
+//array.filter
+//array.every
+//array.some
+//array.sort > a - b asc and b - a desc
+
+//Reduce > resulting in a single value
+//tally
