d17e2249f874a2182adba1abc2b8b6ef44e1c46b
Changappa_m_a <Changappa_m_a@dell.com>
2021-09-03 11:56:39 +0530

    Section 11 complete

---
+++ JavaScript/Section11/otherFeatures.js
@@ -0,0 +1,8 @@
+//default params
+//spread
+const num = [1,2,3,4,5]
+Math.max(...num)
+//Rest > function sum(...num) basically helpful with arrow function
+//destructuring arrays const [gold, silver, bronze] = raceResults;
+//destructring object > const {first, last} = runner;
+
