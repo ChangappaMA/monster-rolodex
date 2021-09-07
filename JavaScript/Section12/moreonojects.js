1e2e5392c4c85748927b765667e7c540ebb5de2c
Changappa_m_a <Changappa_m_a@dell.com>
2021-09-07 13:56:19 +0530

    Section 12 Complete

---
+++ JavaScript/Section12/moreonojects.js
@@ -0,0 +1,20 @@
+//shorthand Properties
+//computed properties
+const team = {
+    [role]: person,
+    [1 + 6 + 5]: "something"
+}
+//methods inside object
+const math = {
+    add: function(x,y){
+        return x,y
+    },
+    multiply: (x, y) => x * y,
+    division(x,y){
+        return x/y
+    } //shorthand
+}
+
+//THIS /this > reference to current execution scope or reference to global scope
+//depends on innvocation context
+//
