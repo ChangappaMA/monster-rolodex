7e57d5d8f05e9766b644c22983a18834bc88969b
Changappa_m_a <Changappa_m_a@dell.com>
2021-09-07 16:40:25 +0530

    Section 13 Completed

---
+++ JavaScript/Section13/DOMMan.js
@@ -0,0 +1,11 @@
+//DOM > Document object Model
+//Javascript representatoin of webpage
+//objects that can be interacted with
+//Document > contains representation of all objects 
+//Selecting 
+//> getElementById("id name")
+//> getElementsByTagName("element name") > Collection of Elements
+//> getElementsByClassName(" ") > collection of Elements > HTMLCollection
+
+//querySelector
+//querySelectotAll > collection > NodeList 
