54900c3151be19052a26f6720d25e188afb2bfb4
Changappa_m_a <Changappa_m_a@dell.com>
2021-09-16 16:04:30 +0530

    Section 16 Complete

---
+++ JavaScript/Section16/AsynchCodeCallPro.js
@@ -0,0 +1,6 @@
+//the Call Stack > feature of JS > Last in First out
+//JS is single threaded language
+//Async Calls
+//Promises > object representing eventual completion or failure of an asiynch operation
+//new Promise() resolve() and reject() // .then() or .catch()
+
