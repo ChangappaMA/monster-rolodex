9637a2171de2455f5a2d867a23768026ca9ca9c9
Changappa_m_a <Changappa_m_a@dell.com>
2021-12-13 19:37:18 +0530

    Adding files to github pages

---
+++ React/testpractice/src/App.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+import './App.css';
+
+import {CardList} from './components/card-list/card-list.component'
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters : [],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json()
+    .then(users => this.setState({ monsters : users})))
+  }
+  
+  render() {
+    const {monsters, searchField} = this.state
+    const Filteredmonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className='App'>
+        <CardList monsters={Filteredmonsters} />        
+      </div>
+    )
+  }
+
+}
+
+export default App;
